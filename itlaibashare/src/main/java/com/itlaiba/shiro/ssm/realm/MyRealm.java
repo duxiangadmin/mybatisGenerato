4a6d29d9e3ff46984e7bcac25089e0fb5a07eedd
admin <937220832@qq.com>
2018-06-19 11:23:53 +0800

    加入了shiro，暂时shiro没有做任何的处理，只是整合了。urls没有写

---
+++ itlaibashare/src/main/java/com/itlaiba/shiro/ssm/realm/MyRealm.java
@@ -0,0 +1,24 @@
+package com.itlaiba.shiro.ssm.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+public class MyRealm extends AuthorizingRealm{
+	
+	
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		return null;
+	}
+
+}
